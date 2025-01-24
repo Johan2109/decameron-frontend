a49b78649a1263b9e600870f5050697cf8a84324
Johan Lopez <johanronaldolopezmontes@gmail.com>
2025-01-24 16:25:21 -0500

    Add xsrftoken

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,7 @@ const baseURL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
 const axiosInstance = axios.create({
   baseURL,
+  withXSRFToken: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
